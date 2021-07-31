283ed4d016b9b76c53c5b3484ea543aeb0f3910f
Gabriel Krzyzanovski dos Santos Neiva <gabrielksneivagi@gmail.com>
2021-07-31 18:46:29 -0300

    Algumas novas operações aritméticas

--- core/views.py
+++ core/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-def hello(request, nome):
-    return HttpResponse(f'<h1>Hello {nome}</h1>')
+def hello(request):
+    return HttpResponse(f'<h1>Hello World</h1>')
+def soma(request, num1, num2):
+    soma = num1+num2
+    return HttpResponse(f'<h1>A soma dos valores digitados é: {soma}</h1>')
+def subtração(request, num1, num2):
+    sub = num1-num2
+    return HttpResponse(f'<h1>A subtração dos valores digitados é: {sub}</h1>')
+def multiplicação(request, num1, num2):
+    multi = num1*num2
+    return HttpResponse(f'<h1>A multiplicação dos valores digitados é: {multi}</h1>')
+def divisão(request, num1, num2):
+    div = num1/num2
+    return HttpResponse(f'<h1>A divisão dos valores digitados é: {div}</h1>')
--- hello_django/urls.py
+++ hello_django/urls.py
@@ -18,5 +18,9 @@ from django.urls import path
 from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('hello/<nome>/',views.hello)
+    path('hello/<nome>/',views.hello),
+    path('soma/<int:num1>/<int:num2>',views.soma),
+    path('sub/<int:num1>/<int:num2>',views.subtração),
+    path('multi/<int:num1>/<int:num2>',views.multiplicação),
+    path('divi/<int:num1>/<int:num2>',views.divisão)
 ]
